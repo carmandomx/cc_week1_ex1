241b4796a247e1c334ec95306cf68d6fd8434f81
EstrellaPoot <Estrella.Poot@itksquare.edu.mx>
2022-09-19 17:28:14 -0500

    complete function to convert celsius to fahrenheit

--- exercise.js
+++ exercise.js
@@ -12,13 +12,12 @@
 const cToF = (c) => {
     /* Only make changes below this comment */
     
-
+    // Declare a constant and assign to it the expresion to calculate Fahrenheit degrees using the parameter received
+    const cToFahrenheit = c * (9 / 5) + 32;
+    // Return the calculated value
+    return cToFahrenheit;
     /* Only make changes below this comment */
-}
-
-
-
-
+};
 
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.cToF = cToF;
