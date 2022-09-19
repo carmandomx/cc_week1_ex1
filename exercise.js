fccd9fff0eba620dc5efa06721c7dc0650409ae3
Karla Chable <Karla.Chable@itksquare.edu.mx>
2022-09-19 15:17:57 -0500

    validation for cToF

--- exercise.js
+++ exercise.js
@@ -9,12 +9,19 @@
 /** DO   CHANGE THE FUNCTION NAME **/
 const cToF = (c) => {
   /* Only make changes below this comment */
-  let farh = (c * 9) / 5 + 32;
-  return farh;
+  if (typeof c === "number" && c !== undefined) {
+    let farh = (c * 9) / 5 + 32;
+    return farh;
+  } else {
+    return "Please only number for 'c'";
+  }
   /* Only make changes below this comment */
 };
 
+/*console.log(cToF());
 console.log(cToF(100));
+console.log(cToF(0));
+console.log(cToF("100"));*/
 
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.cToF = cToF;
