c43e4dbd65df59eeadf7f3066a65e412f310da73
Rodrigo Leon <rodrigo.leon@itksquare.edu.mx>
2022-09-19 16:04:01 -0500

    Excersice completed

--- exercise.js
+++ exercise.js
@@ -6,19 +6,14 @@
     Good luck!
 */
 
-
-
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const cToF = (c) => {
-    /* Only make changes below this comment */
-    
+  /* Only make changes below this comment */
+  //Do the conversion and return the result
+  return (c * 9) / 5 + 32;
 
-    /* Only make changes below this comment */
-}
-
-
-
-
+  /* Only make changes below this comment */
+};
 
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.cToF = cToF;
