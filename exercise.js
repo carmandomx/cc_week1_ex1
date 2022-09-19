4003a70718e0dd8549744d1515bf95b6e6e125e2
LuisVazquezKsquare <luis.vazquez@itksquare.edu.mx>
2022-09-19 15:28:42 -0500

    Completed exercise 2 with comments

--- exercise.js
+++ exercise.js
@@ -11,6 +11,11 @@
 /** DO   CHANGE THE FUNCTION NAME **/
 const cToF = (c) => {
     /* Only make changes below this comment */
+
+    /* This problem can be solved with a single line of code
+    as is not necesary to modify anything and just return the
+    raw result*/
+    /* If necesary we can create a var that saves the value and then change it´s value with the formula*/
     return ((c*9/5)+32);
     /* Only make changes below this comment */
 }
