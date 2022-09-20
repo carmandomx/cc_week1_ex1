c22a581b33e7760a1085e98cb06c3d70c48ea400
Karla Chable <Karla.Chable@itksquare.edu.mx>
2022-09-20 10:14:46 -0500

    delete console.log lines

--- exercise.js
+++ exercise.js
@@ -23,11 +23,5 @@ const cToF = (c) => {
 
   /* Only make changes below this comment */
 };
-
-/*console.log(cToF());
-console.log(cToF(100));
-console.log(cToF(0));
-console.log(cToF("100"));*/
-
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.cToF = cToF;
