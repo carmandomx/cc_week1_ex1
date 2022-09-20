bda83c66c6a201cc95a42d5a354ac6a624be9a05
Brandon Hernandez <Brandon.Hernandez@itksquare.edu.mx>
2022-09-20 09:44:51 -0500

    added additional comments

--- exercise.js
+++ exercise.js
@@ -11,11 +11,11 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const cToF = (c) => {
     /* Only make changes below this comment */
-    let f= c *9/5 + 32; //I created a new variable that will be the temperature un f (farenheit) and applied the formula
-    console.log(f); // I console logged the solution, I could also have used the return function
+    let f = c *9/5 + 32; //I created a new variable that will be the temperature un f (farenheit) and applied the formula
+    console.log(f); // I console logged the solution, I could also have used the return keyword
     /* Only make changes below this comment */
 }
-cToF(10);
+cToF(10); // callback of the function with the temperature in celsius, the output will be in farenheit
 
 
 
