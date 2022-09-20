ef80d5f6c6b1327790bb8563672be49123fbe7aa
Jacinto De la Hoz ITK <113387069+JacintoDelahoz-Ksquare@users.noreply.github.com>
2022-09-20 11:23:33 -0400

    First commit

--- exercise.js
+++ exercise.js
@@ -27,26 +27,3 @@ cToF(0);
 
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.cToF = cToF;
-=======
-/*
-    INSTRUCTIONS
-    This function (cToF) will receive a number as input, this will be in celsius and you need to convert to fahrenheit.
-    The formula that you need to use is C * 9/5 + 32
-
-    Good luck!
-*/
-
-/** DO NOT CHANGE THE FUNCTION NAME **/
-const cToF = (c) => {
-    /* Only make changes below this comment */
-    
-      let Fahr = c * 9 / 5 + 32;
-      return Fahr
-    
-     /* Only make changes below this comment */
-    }
-cToF(0);
-
-/** DO NOT CHANGE THE LINE BELOW **/
-module.exports.cToF = cToF;
->>>>>>> 69329967be0f59f550b3fffea96bb2e6f3676e72
