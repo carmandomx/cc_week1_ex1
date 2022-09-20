5613a3bc1cd008e3239ad8e1c4b7625351b319fe
Jacinto De la Hoz ITK <113387069+JacintoDelahoz-Ksquare@users.noreply.github.com>
2022-09-20 10:32:26 -0400

    Add files via upload

--- exercise.js
+++ exercise.js
@@ -6,19 +6,16 @@
     Good luck!
 */
 
-
-
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const cToF = (c) => {
     /* Only make changes below this comment */
     
-
-    /* Only make changes below this comment */
-}
-
-
-
-
+      let Fahr = c * 9 / 5 + 32;
+      return Fahr
+    
+     /* Only make changes below this comment */
+    }
+cToF(0);
 
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.cToF = cToF;
