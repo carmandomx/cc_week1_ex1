25cd5080d13ad031345ecfe9e7829c59963365a7
Cesar Perez <cesar.perez@itksquare.edu.mx>
2022-09-20 16:19:26 -0400

    Exercise I - C to F. CesarPerez Initial Commit

--- exercise.js
+++ exercise.js
@@ -11,8 +11,24 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const cToF = (c) => {
     /* Only make changes below this comment */
-    
 
+    // Creation of local variables used through this function.
+    const cTemp = c;
+    let Farenheit = 0;
+
+    /* Using the switch control flow to decide between wheter the parameter 
+       given is 0°C which would return the value of 32°F, or any other value 
+       in °C where the formula °F=°C*(9/5)+32. */
+
+    switch (cTemp) {
+        case 0:
+            Farenheit = 32;
+            break;
+        default:
+            Farenheit = cTemp * (9/5) + 32;
+            break;
+    }
+    return Farenheit;
     /* Only make changes below this comment */
 }
 
