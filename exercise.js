009d4694c04f660785a404d76c760d3ab01f4a33
Junior Cabrera <junior.cabrera@itksquare.edu.mx>
2022-09-20 08:20:53 -0400

    Exercise I - C to F - Completed

--- exercise.js
+++ exercise.js
@@ -11,7 +11,14 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const cToF = (c) => {
     /* Only make changes below this comment */
-    
+
+    /*First I declare the variable f (as a reference to fahrenheit) using let to disallow other 
+    declarations of that variable. In the same line I proceed to do the given calculation for the 
+    conversion from Celsius to Fahrenheit storing the result in the variable f. */
+
+    let f = (c * (9/5)) + 32;
+
+    return f; //Finally I return the value of f (our result).
 
     /* Only make changes below this comment */
 }
